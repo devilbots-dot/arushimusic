4dcf34ee73d08256dba61d360e04498dd4140c8f
devilbots-dot <akgtestfactm@gmail.com>
2026-06-14 17:53:27 +0530

    Update bot.py

--- SONALI/core/bot.py
+++ SONALI/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class RAUSHAN(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info("Starting Bot...")
         super().__init__(
             name="SONALI",
             api_id=config.API_ID,
@@ -20,6 +20,7 @@ class RAUSHAN(Client):
 
     async def start(self):
         await super().start()
+
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
@@ -28,28 +29,28 @@ class RAUSHAN(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-
         except Exception as ex:
             LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+                f"Bot has failed to access the log group/channel.\nReason: {type(ex).__name__}"
             )
 
         try:
-    a = await self.get_chat_member(config.LOGGER_ID, self.id)
-    if a.status != ChatMemberStatus.ADMINISTRATOR:
-        LOGGER(__name__).error(
-            "Please promote your bot as an admin in your log group/channel."
-        )
-     except Exception as ex:
-        LOGGER(__name__).error(
-            f"LOGGER_ID Check Failed: {ex}"
-    )
+            a = await self.get_chat_member(config.LOGGER_ID, self.id)
+
+            if a.status != ChatMemberStatus.ADMINISTRATOR:
+                LOGGER(__name__).error(
+                    "Please promote your bot as an admin in your log group/channel."
+                )
+
+        except Exception as ex:
+            LOGGER(__name__).error(
+                f"LOGGER_ID Check Failed: {ex}"
             )
 
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
